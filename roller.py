74341dada331e5e91f9c2da07e76a94fcd8e8c20
blazebissar <25300692+blazebissar@users.noreply.github.com>
2019-11-24 16:13:23 -0500

    Added basic support for attacks

--- roller.py
+++ roller.py
@@ -12,12 +12,73 @@ def get_modifier(value):
     '''
     return (value - 10) // 2
 
+def perform_attack(args, character):
+    '''
+    Parses the commands ands executes the attack. Assumes a strength-based
+    attack unless specified otherwise.
+    '''
+    # Variables to test for commands and their shorthands, also track modifications
+    advantage_types = ['advantage', 'adv']
+    disadvantage_types = ['disadvantage', 'dis']
+    base_skill = 'strength'
+    advantage_status = ''
+
+    print(args)
+
+    # When only specifying advantage/disadvantage or a non-strength stat to use
+    if len(args) == 1:
+        if args[0] in advantage_types + disadvantage_types:
+            advantage_status = args[0]
+        elif args[0] in character.traits:
+            base_skill = args[0]
+        else:
+            print('Error: expected advantage/disadvantage or base skill')
+            return
+    # When using both advantage/disadvantage and a non-strength stat
+    # because we allow for either order in the command
+    elif len(args) == 2:
+        all_words = advantage_types + disadvantage_types + list(character.traits.keys())
+        if args[0] not in all_words or args[1] not in all_words:
+            print('Error: expected advantage/disadvantage or base skill')
+            return
+        if args[0] in advantage_types + disadvantage_types:
+            advantage_status = args[0]
+            base_skill = args[1]
+        else:
+            advantage_status = args[1]
+            base_skill = args[0]
+    elif len(args) != 0:
+        print('Error: expecting zero, one, or two modifiers for attacks')
+        return
+
+    # Perform the attack
+    modifier = get_modifier(int(character.traits[base_skill]))
+    modifier += int(character.traits['proficiency'])
+
+    res, nat_res = utils.skill_check(modifier)
+
+    # Roll again for advantage or disadvantage
+    if advantage_status in advantage_types:
+        re_res, re_nat_res = utils.skill_check(modifier)
+        res, nat_res = max(res, re_res), max(nat_res, re_nat_res)
+    elif advantage_status in disadvantage_types:
+        re_res, re_nat_res = utils.skill_check(modifier)
+        res, nat_res = min(res, re_res), min(nat_res, re_nat_res)
+
+    print('Attack rolled a %d with a natural %d' % (res, nat_res))
+
+
 def handle_skill(args, character):
     '''
     Determines what kind of skill check is required and loads the appropriate
     character information to deal with it. Prints the information directly
     rather than returning.
     '''
+
+    # Variables to test for commands and their shorthands
+    advantage_types = ['advantage', 'adv']
+    disadvantage_types = ['disadvantage', 'dis']
+    check_types = ['save', 'check']
 
     # A properly formatted command will have the skill and either 'check' or
     # 'save' followed by an optional advantage/disadvantage
@@ -30,15 +91,15 @@ def handle_skill(args, character):
         skill, variant, advantage_status = args
         reroll = True
 
-        if advantage_status not in ['advantage', 'disadvantage']:
-            print('Error: expecting third argument to be "advantage" or "disadvantage"')
+        if advantage_status not in advantage_types + disadvantage_types:
+            print('Error: third argument must be one of:', advantage_types + disadvantage_types)
             return
     else:
         print('Error: expecting two or three arguments')
         return
 
-    if variant not in ['save', 'check']:
-        print('Error: expecting second argument to be "check" or "save"')
+    if variant not in check_types:
+        print('Error: second argument must be one of:', check_types)
         return
 
     # Check for proficiency
@@ -53,7 +114,7 @@ def handle_skill(args, character):
     if reroll:
         re_res, re_nat_res = utils.skill_check(modifier)
 
-        if advantage_status == 'advantage':
+        if advantage_status in advantage_types:
             res, nat_res = max(res, re_res), max(nat_res, re_nat_res)
         else:
             res, nat_res = min(res, re_res), min(nat_res, re_nat_res)
@@ -113,7 +174,8 @@ def process_args(args, character='character.txt'):
     # Otherwise it is a command
     elif first in commands:
         if first == 'attack':
-            print('Pending implementation')
+            player_char = utils.Character.from_file(character)
+            perform_attack(args[1::], player_char)
         elif first == 'damage':
             print('Pending implementation')
         elif first == 'gen':
